64cc0aa616743b67fade3ee5f91697bbee96482d
Oscar Morris <twocap06@gmail.com>
2021-05-02 16:50:51 +0100

    added testing scripts

--- process_data.py
+++ process_data.py
@@ -2,14 +2,15 @@ import pytesseract
 import pdf2image
 import PIL
 
-pdf2image.convert_from_path("Data/01.pdf")[0].save("01.jpg", "JPEG")
+def pdf(file):
+    pdf2image.convert_from_path(file)[0].save("process.jpg", "JPEG")
 
-text = str(pytesseract.image_to_string(PIL.Image.open("01.jpg"))).split("\n")
-text_without_spaces = []
-for line in text:
-    if not (line.isspace() or line == ""):
-        text_without_spaces.append(line)
+    text = str(pytesseract.image_to_string(PIL.Image.open("01.jpg"))).split("\n")
+    text_without_spaces = []
+    for line in text:
+        if not (line.isspace() or line == ""):
+            text_without_spaces.append(line)
 
-processed_text = " ".join(text_without_spaces[6:-3])
+    return " ".join(text_without_spaces[6:-3])
 
-print(processed_text)
+print(pdf("Data/sample.pdf"))
